012b52581118f66f10468d8ea982c594788e7738
Laïla Blomme <lblm@reoveiling.be>
2024-10-23 14:09:37 +0200

    cleanup

--- Laila.Shell/Folder.vb
+++ Laila.Shell/Folder.vb
@@ -233,7 +233,6 @@ Public Class Folder
                         Finally
                             If Not IntPtr.Zero.Equals(ptr2) Then Marshal.Release(ptr2)
                         End Try
-                        Dim count As Integer = 0
                         If Not enumShellItems Is Nothing Then
                             Dim shellItems(0) As IShellItem, fetched As UInt32 = 1
                             enumShellItems.Next(1, shellItems, fetched)
@@ -243,10 +242,7 @@ Public Class Folder
                                 Dim fullPath As String = Item.GetFullPathFromShellItem2(shellItems(0))
                                 Dim newItem As Item
                                 If CBool(attr2 And SFGAO.FOLDER) Then
-                                    'UIHelper.OnUIThread(
-                                    '        Sub()
                                     newItem = makeNewFolder(shellItems(0))
-                                    'End Sub)
                                 Else
                                     newItem = makeNewItem(shellItems(0))
                                 End If
@@ -262,8 +258,6 @@ Public Class Folder
                                     End If
                                 End If
                                 enumShellItems.Next(1, shellItems, fetched)
-                                'If count Mod 100 = 0 Then Thread.Sleep(1)
-                                count += 1
                             End While
                         End If
                     Catch ex As Exception
