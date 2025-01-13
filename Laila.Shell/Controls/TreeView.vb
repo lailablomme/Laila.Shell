374f697415f70d4a97e42c1cef6528720f245b04
Laïla Blomme <laila.blomme@hotmail.com>
2025-01-13 05:30:09 +0100

    fix setselectedfolder

--- Laila.Shell/Controls/TreeView.vb
+++ Laila.Shell/Controls/TreeView.vb
@@ -450,7 +450,9 @@ Namespace Controls
                     If Not currentFolder Is Nothing Then
                         While Not currentFolder Is Nothing _
                         AndAlso Not noRecursive.Contains(currentFolder.Pidl.ToString()) _
-                        AndAlso (Me.DoShowAllFoldersInTreeView OrElse Not currentFolder.Parent Is Nothing)
+                        AndAlso (Me.DoShowAllFoldersInTreeView _
+                                 OrElse currentFolder.TreeRootIndex <> -1 _
+                                 OrElse Not currentFolder.Parent Is Nothing)
                             noRecursive.Add(currentFolder.Pidl.ToString())
                             list.Add(currentFolder)
                             Debug.WriteLine("SetSelectedFolder Added parent " & currentFolder.FullPath)
