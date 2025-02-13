7b377c2efd29fa63ef9fccb3d9d480027055e751
Laila Blomme <LBLM@reoveiling.be>
2025-02-13 11:40:44 +0100

    enum desktop fix

--- Laila.Shell/Link.vb
+++ Laila.Shell/Link.vb
@@ -18,7 +18,10 @@ Public Class Link
         Shell.GlobalThreadPool.Run(
             Sub()
                 CType(shellItem2, IShellItem2ForShellLink).BindToHandler(Nothing, Guids.BHID_SFUIObject, GetType(IShellLinkW).GUID, _shellLinkW)
-                _shellLinkW.Resolve(IntPtr.Zero, SLR_FLAGS.NO_UI Or SLR_FLAGS.NOSEARCH)
+                Try
+                    _shellLinkW.Resolve(IntPtr.Zero, SLR_FLAGS.NO_UI Or SLR_FLAGS.NOSEARCH)
+                Catch ex As NotImplementedException
+                End Try
             End Sub,, _threadId)
     End Sub
 
